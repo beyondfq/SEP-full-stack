0f0d21cc08f1aa56612a4f23c71c90e0a5cf175a
beyondfq <70784963+beyondfq@users.noreply.github.com>
2022-07-19 00:57:12 -0400

    Add files via upload

---
+++ MovieShop/ApplicationCore/RepositoryContracts/IMovieRepository.cs
@@ -0,0 +1,23 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.RepositoryContracts
+{
+    public interface IMovieRepository
+    {
+        // CRUD operations regarding Movie Table
+        // use Movie Entity
+
+        // method to get top 30 highest grossing movies
+
+        List<Movie> GetTop30HighestRevenueMovies();
+
+        List<Movie> GetTop30RatedMovies();
+
+        Movie GetById(int id);
+    }
+}
